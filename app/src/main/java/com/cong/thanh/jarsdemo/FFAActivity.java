da3e302b1fe9bed1f28434ceaa38605434a02a89
LeCuongNexlesoft <cuonglv@nexlesoft.com>
2019-04-07 17:31:32 +0700

    init project

---
+++ app/src/main/java/com/cong/thanh/jarsdemo/FFAActivity.java
@@ -0,0 +1,17 @@
+package com.cong.thanh.jarsdemo;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class FFAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ffa);
+
+    }
+
+}
